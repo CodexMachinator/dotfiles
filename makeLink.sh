4b66cbd8bd9669450f8fd0249199d81aa9e4b012
Michael McConnehey <CodexMachinator@gmail.com>
2020-12-16 17:27:30 -0800

    add makeLink script to create sym links to dot files

---
+++ makeLink.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s "$HOME/.dotfiles/.bash_alias" "$HOME/.bash_alias" 
+ln -s "$HOME/.dotfiles/.bash_color" "$HOME/.bash_color"
+ln -s "$HOME/.dotfiles/.bash_logout" "$HOME/.bash_logout"
+ln -s "$HOME/.dotfiles/.bashrc" "$HOME/.bashrc"
+ln -s "$HOME/.dotfiles/.inputrc" "$HOME/.inputrc"
+ln -s "$HOME/.dotfiles/.profile" "$HOME/.profile"
+
